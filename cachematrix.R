64f1d3fc90c9cd26de24c36ac3afa60212a8b456
arjun8001 <gupta.arjun.rajendra@gmail.com>
2020-12-27 13:46:58 +0530

    assignment completed with comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,10 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-# set the value of the vector
-# get the value of the vector
-# set the value of the mean
-# get the value of the mean
+## Write a short comment describing this function
+## this function creates a list containing set (set a matrix),
+##get (call the matrix), setinverse (set the inverse of matrix)
+##and getinverse (call the inverse of matrix) function 
 
 makeCacheMatrix <- function(myMat = matrix()) {
 
@@ -28,7 +28,10 @@ makeCacheMatrix <- function(myMat = matrix()) {
 }
 
 
-## below function checks if the cached data is present or not. and if it is not present it will re-calculate
+## Write a short comment describing this function
+## This function takes makeCacheMatrix(matrix) as an input
+##and return inverse of the matrix (compute if  inverse of matrix is not cached
+##or else restore from cache)
 
 cacheSolve <- function(myMat, ...) {
         ## Return a matrix that is the inverse of 'myMat'
